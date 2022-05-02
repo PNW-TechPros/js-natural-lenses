f4ff6eb91e94f4c519bfb8fc028f622449519996
Richard T. Weeks <rtweeks21@gmail.com>
2022-05-02 09:20:49 -0700

    Improve documentation
    Issues: #2

--- src/sugar.js
+++ src/sugar.js
@@ -10,7 +10,10 @@ import { Parser, states, actions } from '../src-cjs/parser.js';
  * are the single dot (`.`) and the square brackets (`[...]`); all other
  * operators would result in a non-Lens optic.  Within the square brackets,
  * only string literals (in single- or double-quotes), unsigned or negative
- * integers, and intercalated values are allowed.
+ * integers, and intercalated values are allowed.  Use of unquoted `@` (the
+ * JSONPath *current object/element*) in the expression is not allowed, and the
+ * `$` (the JSONPath *root object*) is only allowed — and required — as the
+ * first character of the expression.
  *
  * When an intercalated value is used within a subscript operator, the actual
  * JavaScript value — not its string representation — is used as the step in
@@ -18,10 +21,13 @@ import { Parser, states, actions } from '../src-cjs/parser.js';
  * for custom stepping or arbitrary JavaScript values for keys into a Map.
  *
  * This template tag processes the raw strings used in the template (to avoid
- * doubling of backslashes for escape sequences), so use `\x60` as an escape for
- * the backtick rather than a backslash-backtick combination.  Such characters
- * are — due to the JSONPath syntax — only allowable inside a string literal
- * within square brackets.
+ * doubling of backslashes for escape sequences); though this means a
+ * backslash-backtick combination (since backtick by itself ends the template)
+ * is processed as two characters, the only valid context for this to occur —
+ * due to the JSONPath syntax — is inside a string literal within square
+ * brackets, in which case the backslash-backtick sequence will be interpreted
+ * as a single backtick anyway.  If this causes confusion, the `\x60` escape
+ * sequence can be used instead.
  */
 export default function lensFromJSONPath(stringParts, values) {
   const stringsCursor = stringParts.raw[Symbol.iterator]();
