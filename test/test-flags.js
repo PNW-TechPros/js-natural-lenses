2642b8540dc87cbaedb80a248e80fe6d2e7644d4
Richard T. Weeks <rtweeks21@gmail.com>
2022-05-18 07:09:24 -0700

    Test correctness of datum plan property version locking
    When run with `datumPlanApiVersions` in the `TEST_FLAGS` envvar, this
    will install each prior minor version of this package in a temporary
    directory and get a list of all property names defined on a `Lens`, then
    check that the names in the current version *not* in the prior version
    are listed as the "weak" names for the prior version (within the current
    version).
    
    Issues: #27

---
+++ test/test-flags.js
@@ -0,0 +1,10 @@
+
+const FLAGS_GIVEN = new Set(
+  (process.env.TEST_FLAGS || '')
+  .split(/\s*,\s*/g)
+  .filter(f => !f.match(/^\s*$/))
+);
+
+exports.include = FLAGS_GIVEN.has.bind(FLAGS_GIVEN);
+
+exports.DATUM_PLAN_LENS_API_VERSIONS = 'datumPlanApiVersions';
